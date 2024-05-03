8835fc0ef5821819db14cadf78a401b8041e2256
ppennyy <12750245@me.mcu.edu.tw>
2024-05-03 11:48:19 +0800

    week11

---
+++ week11/week11-4a.cpp
@@ -0,0 +1,13 @@
+//week11-4a.cpp
+#include <stdio.h>
+int main()
+{
+	int a[10][10];
+	int m,n;
+	scanf("%d %d",&m,&n);
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+}
